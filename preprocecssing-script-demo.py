8ebb6bb4c5722939d8e3605ac2ed931c94358d91
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2022-11-17 19:20:22 +0530

    Update preprocecssing-script-demo.py

--- preprocecssing-script-demo.py
+++ preprocecssing-script-demo.py
@@ -4,7 +4,7 @@ s= "this is captain speaking!"
 # with open ('/__w/ml-sagemaker/ml-sagemaker/details.txt','r') as file:
 cwd = os.getcwd()
 
-with open (os.path.join(cwd , 'details.txt' )) as file:
+with open ('details.txt','w') as file:
     output = file.write(s)
     
 print(output)
